d87c5431b98f1548c9d33a28898e75ab2ca3c61c
Ashley Ng <ashleyng@users.noreply.github.com>
2019-02-03 13:05:08 -0600

    add documentation

--- Source/Model/Participant.swift
+++ Source/Model/Participant.swift
@@ -31,14 +31,22 @@ public struct Participant: Codable {
     public let tournamentId: Int
     public let icon: String?
     private let groupPlayerIdsArray: [Int]
+    /// a participant can have multiple Ids when the tournament
+    /// is a multi-stage tournament (group stage and then bracket).
+    /// `main` is their top level Id,
+    /// `group` is the Ids the participant can have in the group stages.
+    /// `all` is an array of all the participants Ids
     public let id: Id
     
+    /// Custom decoder to properly setup the
+    /// participant's multiple Ids.
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         mainId = try values.decode(Int.self, forKey: .mainId)
         name = try values.decode(String.self, forKey: .name)
         tournamentId = try values.decode(Int.self, forKey: .tournamentId)
         icon = try values.decodeIfPresent(String.self, forKey: .icon)
+
         groupPlayerIdsArray = try values.decode([Int].self, forKey: .groupPlayerIdsArray)
         var groupIdCopy = groupPlayerIdsArray
         groupIdCopy.append(mainId)
