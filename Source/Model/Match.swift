649e98c4ec40e9c076744f99fb2b763247a3dc20
Ashley Ng <ashleyng@users.noreply.github.com>
2019-03-08 22:24:08 -0600

    Get match pre-req information

--- Source/Model/Match.swift
+++ Source/Model/Match.swift
@@ -11,11 +11,27 @@ public struct RootMatch: Codable {
     let match: Match
 }
 
+
+
 public struct Match: Codable {
     public enum State: String, Codable {
         case open
         case pending
         case complete
+    }
+    
+    public struct PreReqInformation: Codable {
+        enum CodingKeys: String, CodingKey {
+            case player1MatchId = "player1_prereq_match_id"
+            case player2MatchId = "player2_prereq_match_id"
+            case player1MatchIsLoser = "player1_is_prereq_match_loser"
+            case player2MatchIsLoser = "player2_is_prereq_match_loser"
+        }
+        
+        public let player1MatchId: Int?
+        public let player2MatchId: Int?
+        public let player1MatchIsLoser: Bool
+        public let player2MatchIsLoser: Bool
     }
     
     enum CodingKeys: String, CodingKey {
@@ -29,6 +45,7 @@ public struct Match: Codable {
         case player1Votes = "player1_votes"
         case player2Votes = "player2_votes"
         case groupId = "group_id"
+        
     }
     
     public let id: Int
@@ -43,6 +60,7 @@ public struct Match: Codable {
     public let player2Votes: Int?
     public let groupId: Int?
     public let round: Int
+    public let preReqInfo: PreReqInformation
     
     /// Creates a dictionary mapped from the participants
     /// main Id to their score
@@ -53,6 +71,20 @@ public struct Match: Codable {
         return [player1Id: String(splitScore[0]), player2Id: String(splitScore[1])]
     }
     
+    public var playerOneScore: String? {
+        guard let splitScore = scoresCsv?.split(separator: "-"), splitScore.count > 0 else {
+            return nil
+        }
+        return String(splitScore[0])
+    }
+    
+    public var playerTwoScore: String? {
+        guard let splitScore = scoresCsv?.split(separator: "-"), splitScore.count > 0 else {
+            return nil
+        }
+        return String(splitScore[1])
+    }
+    
     /// Creates a dictionary mapped from the participants
     /// main Id to the number of votes they received
     public var votes: Dictionary<Int, Int>? {
@@ -61,6 +93,30 @@ public struct Match: Codable {
         }
         
         return [player1Id: player1Votes, player2Id: player2Votes]
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int.self, forKey: .id)
+        player1Id = try values.decodeIfPresent(Int.self, forKey: .player1Id)
+        player2Id = try values.decodeIfPresent(Int.self, forKey: .player2Id)
+        state = try values.decode(State.self, forKey: .state)
+        tournamentId = try values.decode(Int.self, forKey: .tournamentId)
+        winnerId = try values.decodeIfPresent(Int.self, forKey: .winnerId)
+        scoresCsv = try values.decodeIfPresent(String.self, forKey: .scoresCsv)
+        suggestedPlayOrder = try values.decodeIfPresent(Int.self, forKey: .suggestedPlayOrder)
+        player1Votes = try values.decodeIfPresent(Int.self, forKey: .player1Votes)
+        player2Votes = try values.decodeIfPresent(Int.self, forKey: .player2Votes)
+        groupId = try values.decodeIfPresent(Int.self, forKey: .groupId)
+        round = try values.decode(Int.self, forKey: .round)
+        
+        let preReqValues = try decoder.container(keyedBy: PreReqInformation.CodingKeys.self)
+        let preReqPlayer1Match = try preReqValues.decodeIfPresent(Int.self, forKey: .player1MatchId)
+        let preReqPlayer2Match = try preReqValues.decodeIfPresent(Int.self, forKey: .player2MatchId)
+        let preReqPlayer1IsLoser = try preReqValues.decode(Bool.self, forKey: .player1MatchIsLoser)
+        let preReqPlayer2IsLoser = try preReqValues.decode(Bool.self, forKey: .player2MatchIsLoser)
+        
+        preReqInfo = PreReqInformation(player1MatchId: preReqPlayer1Match, player2MatchId: preReqPlayer2Match, player1MatchIsLoser: preReqPlayer1IsLoser, player2MatchIsLoser: preReqPlayer2IsLoser)
     }
     
     public init(id: Int,
@@ -74,7 +130,8 @@ public struct Match: Codable {
                 player1Votes: Int?,
                 player2Votes: Int?,
                 groupId: Int?,
-                round: Int) {
+                round: Int,
+                preReqInfo: PreReqInformation) {
         self.id = id
         self.player1Id = player1Id
         self.player2Id = player2Id
@@ -87,5 +144,6 @@ public struct Match: Codable {
         self.player2Votes = player2Votes
         self.groupId = groupId
         self.round = round
+        self.preReqInfo = preReqInfo
     }
 }
